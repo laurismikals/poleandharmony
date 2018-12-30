5959e78a322f02a45ebe410b2de7ceb71ba1efaf
Lauris <lauris.mikals@gmail.com>
2018-12-30 12:16:27 +0200

    Switch to different db

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const reactViews = require('express-react-views');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/nodekb', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/poleandharmony', { useNewUrlParser: true });
 const db = mongoose.connection;
 
 db.once('open', () => console.log('Connected to MongoDB...'));
