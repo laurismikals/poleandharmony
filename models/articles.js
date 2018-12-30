f60ac90233047aca50cef37f72a929572573edaf
Lauris <lauris.mikals@gmail.com>
2018-12-30 11:36:44 +0200

    Add some basic server files

---
+++ models/articles.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const articleSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+});
+
+const Articles = mongoose.model('Article', articleSchema);
+
+module.exports = Articles;
---
+++ routes/articles.js
@@ -0,0 +1,45 @@
+const express = require('express');
+
+const router = express.Router();
+const Articles = require('../models/articles.js');
+
+router.get('/add', (req, res) => {
+  res.render('articles_add', { title: 'Add article' });
+});
+
+router.post('/add', async (req, res) => {
+  const { title, author, body } = req.body;
+  let article = new Articles({ title, author, body });
+
+  try {
+    await article.save();
+    res.redirect('/');
+  } catch (e) { console.error(e); }
+});
+
+router.get('/edit/:id', async (req, res) => {
+  try {
+    const article = await Articles.findById(req.params.id);
+    res.render('articles_edit', { title: 'Edit article', article });
+  } catch (e) { console.error(e); }
+});
+
+router.post('/edit/:id', async (req, res) => {
+  const { title, author, body } = req.body;
+
+  const query = { _id: req.params.id };
+
+  try {
+    await Articles.updateOne(query, { title, author, body });
+    res.redirect('/');
+  } catch (e) { console.error(e); }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const article = await Articles.findById(req.params.id);
+    res.render('article', { article });
+  } catch (e) { console.error(e); }
+});
+
+module.exports = router;
