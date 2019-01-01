05f7d39da6e2806f59b7064bbad13c0e4b3fef46
Lauris <lauris.mikals@gmail.com>
2019-01-01 22:31:31 +0200

    Add dynamic data to SitetreeAdd view

--- admin/src/views/SitetreeAdd/SitetreeAdd.jsx
+++ admin/src/views/SitetreeAdd/SitetreeAdd.jsx
@@ -1,9 +1,34 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 
-const SitetreeAdd = () => (
-  <Fragment>
-    <h1>Pievienot sadaļu</h1>
-  </Fragment>
-);
+const SitetreeAdd = () => {
+  const [pages, setPages] = useState({});
+
+  useEffect(() => {
+    fetch('/pages')
+      .then(response => response.json())
+      .then(response => setPages(response))
+  }, []);
+
+  return (
+    <Fragment>
+      <h1>Pievienot sadaļu lapas kokam</h1>
+      <form method="post" action="/sitetree/add">
+        <select name="pageId">
+          {Object.keys(pages).map((key, i) => (
+            <option key={i} value={key}>
+              {pages[key].name}
+            </option>
+          ))}
+        </select>
+        <button
+          className="btn btn-primary"
+          type="submit"
+        >
+          Saglabāt
+        </button>
+      </form>
+    </Fragment>
+  );
+};
 
 export default SitetreeAdd;
