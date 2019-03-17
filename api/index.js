1e142e69d4557d6e30cf02fd66c917ffcafec9bb
Lauris <lauris.mikals@gmail.com>
2019-03-17 14:11:24 +0200

    Allow api requests from admin

--- api/index.js
+++ api/index.js
@@ -6,6 +6,14 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', `http://admin.${process.env.DOMAIN}`);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', false);
+  next();
+});
+
 const articles = require('./routes/articles.js');
 const articleCategories = require('./routes/articleCategories.js');
 const sitetree = require('./routes/sitetree.js');
