51950e0dabc5974faa97442ed2312ebe3c5dd739
Lauris <lauris.mikals@gmail.com>
2019-04-07 15:57:50 +0300

    Move article add functionality to Article view

---
+++ admin/src/views/Articles/Add/Add.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+
+export const Add = () => {
+  const [articleCategories, setArticleCategories] = useState([]);
+
+  useEffect(() => {
+    const getArticleCategories = () => fetch(`//api.${process.env.DOMAIN}/articleCategories`)
+      .then(res => res.json())
+      .then(res => setArticleCategories(res));
+
+    getArticleCategories();
+  }, []);
+
+  return (
+    <>
+      <form method="post" action="/articles/add">
+        {articleCategories && (
+          <>
+            <label htmlFor="articleCategories">Rakstu kategorija</label>
+            <select
+              className="form-control"
+              id="articleCategories"
+              name="category"
+            >
+              {articleCategories.map(({ _id, category }) => (
+                <option key={_id} value={_id}>{category}</option>
+              ))}
+            </select>
+          </>
+        )}
+        <input
+          className="form-control"
+          type="text"
+          name="title"
+          placeholder="Virsraksts"
+        />
+        <input
+          className="form-control"
+          type="text"
+          name="author"
+          placeholder="Autors"
+        />
+        <textarea
+          className="form-control"
+          name="body"
+          placeholder="Teksts"
+        />
+        <button
+          className="btn btn-primary"
+          type="submit"
+        >
+          Saglabāt
+        </button>
+      </form>
+      <form method="post" action="/articleCategories/add">
+        <input
+          className="form-control"
+          type="text"
+          name="category"
+          placeholder="Pievienot jaunu kategoriju"
+        />
+        <button
+          className="btn btn-primary"
+          type="submit"
+        >
+          Pievienot
+        </button>
+      </form>
+    </>
+  );
+};
