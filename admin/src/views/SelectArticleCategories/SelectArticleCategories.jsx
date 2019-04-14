a067562c8885d96e9d25f2b148be400626babc6a
Lauris <lauris.mikals@gmail.com>
2019-04-14 20:49:08 +0300

    Improve SelectArticleCategories

--- admin/src/views/SelectArticleCategories/SelectArticleCategories.jsx
+++ admin/src/views/SelectArticleCategories/SelectArticleCategories.jsx
@@ -1,11 +1,17 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Link from 'redux-first-router-link';
+
+import { checkIfDataAvailable } from 'HELPERS/checkIfDataAvailable.js';
 
 import { articleCategoriesFetch } from 'REDUCERS/articleCategories.js';
 
+const DEFAULT_VALUE = '';
+
 export const Select = ({
   articleCategories, fetchArticleCategories, value, onChange,
+  isLoading, isAllDataAvailable,
 }) => {
   useEffect(() => {
     fetchArticleCategories();
@@ -16,29 +22,59 @@ export const Select = ({
       <label htmlFor="articleCategories">
         Rakstu kategorija
       </label>
-      <select
-        className="form-control"
-        id="articleCategories"
-        name="articleCategory"
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-      >
-        {articleCategories.map(({ _id, name }) => (
-          <option key={_id} value={_id}>{name}</option>
-        ))}
-      </select>
+      {isLoading && 'Loading...'}
+      {!isLoading && !isAllDataAvailable && (
+        <>
+          <br />
+          No article categories found.
+          <Link
+            to="/articleCategories/add"
+            className="btn btn-primary"
+          >
+            Pievienot rakstu kategoriju
+          </Link>
+          <br />
+        </>
+      )}
+      {!isLoading && isAllDataAvailable && (
+        <select
+          className="form-control"
+          id="articleCategories"
+          name="articleCategory"
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+        >
+          <option disabled value={DEFAULT_VALUE}>
+            Rakstu kategorija
+          </option>
+          {articleCategories.map(({ _id, name }) => (
+            <option key={_id} value={_id}>{name}</option>
+          ))}
+        </select>
+      )}
     </>
   );
 };
 
 Select.propTypes = {
+  isAllDataAvailable: PropTypes.bool.isRequired,
+  isLoading: PropTypes.bool.isRequired,
   articleCategories: PropTypes.arrayOf(PropTypes.shape).isRequired,
   fetchArticleCategories: PropTypes.func.isRequired,
-  value: PropTypes.string.isRequired,
+  value: PropTypes.string,
   onChange: PropTypes.func.isRequired,
 };
 
-const mapState = ({ articleCategories: { data } }) => ({ articleCategories: data });
+Select.defaultProps = {
+  value: DEFAULT_VALUE,
+};
+
+
+const mapState = ({ articleCategories: { data, isLoading } }) => ({
+  articleCategories: data,
+  isLoading,
+  isAllDataAvailable: checkIfDataAvailable(data),
+});
 const mapDispatch = (dispatch) => ({
   fetchArticleCategories: () => dispatch(articleCategoriesFetch()),
 });
