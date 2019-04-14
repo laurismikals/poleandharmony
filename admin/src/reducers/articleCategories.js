7ddc99ce2fc4afcaf9a1751aa4db4fa6a54d2682
Lauris <lauris.mikals@gmail.com>
2019-04-14 22:46:50 +0300

    Improve reducer

--- admin/src/reducers/articleCategories.js
+++ admin/src/reducers/articleCategories.js
@@ -4,10 +4,16 @@ import { ajax } from 'HELPERS/ajax.js';
 
 const NAMESPACE = 'articleCategories';
 const FETCH = `${NAMESPACE}/FETCH`;
+const ADD = `${NAMESPACE}/ADD`;
+const EDIT = `${NAMESPACE}/EDIT`;
+const DELETE = `${NAMESPACE}/DELETE`;
 const LOAD = `${NAMESPACE}/LOAD`;
 const ERROR = `${NAMESPACE}/ERROR`;
 
 export const articleCategoriesFetch = () => ({ type: FETCH });
+export const articleCategoriesAdd = (payload) => ({ type: ADD, payload });
+export const articleCategoriesEdit = (payload) => ({ type: EDIT, payload });
+export const articleCategoriesDelete = (payload) => ({ type: DELETE, payload });
 export const articleCategoriesLoad = (payload) => ({ type: LOAD, payload });
 export const articleCategoriesError = (payload) => ({ type: ERROR, payload });
 
@@ -56,6 +62,24 @@ function* articleCategoriesFetchSaga() {
   }
 }
 
+const articleCategoriesAdder = (body) => ajax('/articleCategories/add', {
+  method: 'POST',
+  body: JSON.stringify(body),
+  headers: { 'Content-Type': 'application/json' },
+});
+
+function* articleCategoriesAddSaga(action) {
+  try {
+    const response = yield call(articleCategoriesAdder, action.payload);
+    console.log('response', response);
+    // yield put(articleCategoriesLoad(response));
+  } catch (e) {
+    console.error('Error: ', e);
+    yield put(articleCategoriesError(e));
+  }
+}
+
 export function* articleCategoriesSaga() {
   yield takeEvery(FETCH, articleCategoriesFetchSaga);
+  yield takeEvery(ADD, articleCategoriesAddSaga);
 }
