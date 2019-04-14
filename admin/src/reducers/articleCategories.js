c8d48c4d0745eeb92d28ae98d7250b3ed9b706bc
Lauris <lauris.mikals@gmail.com>
2019-04-14 20:51:18 +0300

    Add loading and error states

--- admin/src/reducers/articleCategories.js
+++ admin/src/reducers/articleCategories.js
@@ -5,20 +5,39 @@ import { ajax } from 'HELPERS/ajax.js';
 const NAMESPACE = 'articleCategories';
 const FETCH = `${NAMESPACE}/FETCH`;
 const LOAD = `${NAMESPACE}/LOAD`;
+const ERROR = `${NAMESPACE}/ERROR`;
 
 export const articleCategoriesFetch = () => ({ type: FETCH });
 export const articleCategoriesLoad = (payload) => ({ type: LOAD, payload });
+export const articleCategoriesError = (payload) => ({ type: ERROR, payload });
 
 const initialState = {
   data: [],
+  isLoading: false,
+  error: false,
 };
 
 export default (state = initialState, action = {}) => {
   switch (action.type) {
+    case FETCH: {
+      return {
+        ...state,
+        isLoading: true,
+        error: false,
+      };
+    }
     case LOAD: {
       return {
         ...state,
         data: action.payload,
+        isLoading: false,
+      };
+    }
+    case ERROR: {
+      return {
+        ...state,
+        isLoading: false,
+        error: true,
       };
     }
     default: return state;
@@ -33,6 +52,7 @@ function* articleCategoriesFetchSaga() {
     yield put(articleCategoriesLoad(response));
   } catch (e) {
     console.error('Error: ', e);
+    yield put(articleCategoriesError(e));
   }
 }
 
