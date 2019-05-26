71fbf9d9654a9876410ad2c9c8a8be771692be27
Lauris <lauris.mikals@gmail.com>
2019-05-26 19:00:49 +0300

    Add basic CKEditor implementation

--- admin/src/views/Articles/Edit/Edit.jsx
+++ admin/src/views/Articles/Edit/Edit.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-export const Edit = ({ id }) => (<div>{id}</div>);
+export const Edit = ({ id }) => {
+  const [article, setArticle] = useState('<p>Hello from CKEditor 5!</p>');
+
+  return (
+    <div>
+      {id}
+      <CKEditor
+        editor={ClassicEditor}
+        data={article}
+        onChange={(event, editor) => setArticle(editor.getData())}
+      />
+    </div>
+  );
+};
 
 Edit.propTypes = {
   id: PropTypes.string.isRequired,
