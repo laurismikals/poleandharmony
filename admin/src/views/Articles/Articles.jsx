eb96acd16e2448fc8767b8317e105ef58ec413b4
Lauris <lauris.mikals@gmail.com>
2019-05-26 20:15:28 +0300

    Add proper spacing between heading and rest of the content

--- admin/src/views/Articles/Articles.jsx
+++ admin/src/views/Articles/Articles.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import { ElementSpacer } from 'UI/ElementSpacer/ElementSpacer.jsx';
+
 import { ListConnected } from './List/List.jsx';
 import { AddConnected } from './Add/Add.jsx';
 import { EditConnected } from './Edit/Edit.jsx';
 
 const Articles = ({ payload: { action, id } }) => (
-  <>
+  <ElementSpacer column>
     <h1>Raksti</h1>
     {!action && <ListConnected />}
     {action === 'add' && <AddConnected />}
     {action === 'edit' && <EditConnected id={id} />}
-  </>
+  </ElementSpacer>
 );
 
 Articles.propTypes = {
